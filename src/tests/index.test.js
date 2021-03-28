a99c5d36d649ea762a38f5bbc68f2ab05e422fa1
natsume0718 <moto718program@gmail.com>
2021-03-28 19:40:58 +0900

    add multiply test

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,4 +1,4 @@
-import { add } from '../index';
+import { add, multiply } from '../index';
 
 describe('add', () => {
     test('over 1000', () => {
@@ -25,5 +25,33 @@ describe('add', () => {
     test('normal add', () => {
         const result = add(1, 5)
         expect(result).toBe(6);
+    })
+});
+
+describe('multiply', () => {
+    test('over 1000', () => {
+        const result = multiply(1000, 2)
+        expect(result).not.toBe(2000)
+        expect(result).toBe('big big number')
+    });
+
+    test('multiply max -1 arg', () => {
+        const arr = Array(30).fill(2);
+        const result = multiply(...arr)
+        expect(result).toBe(Math.pow(2, 30))
+    });
+
+    test('multiply max arg', () => {
+        const arr = Array(31).fill(2);
+        expect(() => multiply(...arr)).toThrow("over limit argument")
+    });
+
+    test('pass multiple mold', () => {
+        expect(() => multiply(1, 5, "aa")).toThrow("error only accept number")
+    });
+
+    test('normal multiply', () => {
+        const result = multiply(2, 5)
+        expect(result).toBe(10);
     })
 });
